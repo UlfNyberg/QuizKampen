55a725b9dc35825b1394bdefcec5a2aa1be8ecd3
Elliotkongu <elliot.falt@gmail.com>
2020-11-13 14:22:38 +0100

    Added GetCategories to DAO, returning a list of all categories

--- src/DAO.java
+++ src/DAO.java
@@ -1,8 +1,6 @@
 import QuestionsAndAnswers.Question;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Elliot Åberg Fält
@@ -13,9 +11,10 @@ import java.util.Map;
  */
 public class DAO {
     private LinkedHashMap<String, List<Question>> categories;
-    private FileImporter fileImporter = new FileImporter("QuestionsAndAnswers.txt");
+    private FileImporter fileImporter;
 
-    public DAO() {
+    public DAO(String fileName) {
+        this.fileImporter = new FileImporter(fileName);
         this.categories = fileImporter.readFile();
     }
 
@@ -26,5 +25,25 @@ public class DAO {
             }
         }
         return null;
+    }
+
+    public List<Question> getRandomQuestions (String s, int amount) {
+        List<Question> questions = getCategoryData(s);
+        Random random = new Random();
+        List<Question> randomQuestions = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            int index = random.nextInt(questions.size());
+            randomQuestions.add(questions.get(index));
+            questions.remove(index);
+        }
+        return randomQuestions;
+    }
+
+    public List<String> getCategories () {
+        List<String> categoriesList = new ArrayList<>();
+        for (Map.Entry<String, List<Question>> entry : categories.entrySet()) {
+            categoriesList.add(entry.getKey());
+        }
+        return categoriesList;
     }
 }
