cb964b33ac64e08917ea67bff844357256049936
UlfNyberg <72010004+UlfNyberg@users.noreply.github.com>
2020-11-25 14:24:11 +0100

    förbättrat till Image Icons, third draft

--- src/GUI/HomescreenGUI.java
+++ src/GUI/HomescreenGUI.java
@@ -32,12 +32,25 @@ public class HomescreenGUI extends JPanel {
     ImageIcon bild2 = new ImageIcon(new ImageIcon ("src/GUI/illustration2.jpg").getImage().getScaledInstance(250,850, Image.SCALE_DEFAULT));
     ImageIcon bild3 = new ImageIcon("src/GUI/illustration3.jpg");
     ImageIcon bild4 = new ImageIcon("src/GUI/illustration4.jpg");
-    ImageIcon bild5 = new ImageIcon(new ImageIcon ("src/GUI/logo.jpg").getImage().getScaledInstance(150,150, Image.SCALE_DEFAULT));
+    ImageIcon bild5 = new ImageIcon(new ImageIcon ("src/GUI/logopicture.jpg").getImage().getScaledInstance(220,220, Image.SCALE_DEFAULT));
+    ImageIcon bild6 = new ImageIcon(new ImageIcon ("src/GUI/welcomePicture.jpg").getImage().getScaledInstance(220,340, Image.SCALE_DEFAULT));
+
+
+
+    ImageIcon skyBanner1Image = new ImageIcon(new ImageIcon ("src/GUI/skyBanner1.png").getImage().getScaledInstance(250,850, Image.SCALE_DEFAULT));
+    ImageIcon skyBanner2Image = new ImageIcon(new ImageIcon ("src/GUI/skyBanner2.png").getImage().getScaledInstance(250,850, Image.SCALE_DEFAULT));
+    ImageIcon gameNameBannerImage = new ImageIcon(new ImageIcon ("src/GUI/gameNamebanner.png").getImage().getScaledInstance(150,150, Image.SCALE_DEFAULT));
+    ImageIcon gameNameCenterImage = new ImageIcon(new ImageIcon ("src/GUI/GameNameCenter.png").getImage().getScaledInstance(250,250, Image.SCALE_DEFAULT));
+    ImageIcon gameNameTopImage = new ImageIcon(new ImageIcon ("src/GUI/gameNameTop.png").getImage().getScaledInstance(250,250, Image.SCALE_DEFAULT));
+
+
+    JLabel imagePanel = new JLabel(new ImageIcon("src/GUI/gameNameTop.png"));
+
 
     //JButton imageButton1 = new JButton(new ImageIcon("src/GUI/illustration1.jpg"));
     JButton imageButton1 = new JButton(bild1);
     JButton imageButton2 = new JButton(bild2);
-    JButton imageButton3 = new JButton(bild5);
+    JButton imageButton3 = new JButton(bild6);
 
 
     public HomescreenGUI(ActionListener al) {
@@ -50,7 +63,8 @@ public class HomescreenGUI extends JPanel {
         panel.add(imageButton1, BorderLayout.WEST);welcomeLabel.setSize(30,30);
         panel.add(imageButton2, BorderLayout.EAST);
         panel.add(panel2, BorderLayout.CENTER);
-        panel2.add(welcomeLabel);
+        panel2.add(imageButton3);
+        //panel2.add(welcomeLabel);
         panel2.add(nameOfPlayerLabel);
         panel2.add(nameOfPlayerTextField);
         panel2.add(ipaddressLabel);
@@ -58,9 +72,10 @@ public class HomescreenGUI extends JPanel {
         panel2.add(portNrLabel);
         panel2.add(portNrTextField);
         //panel2.add(imageButton3);
+        //panel.add(new ImageIcon(gameNameTopImage));
         imageButton1.setPreferredSize(new Dimension(80,50));
         imageButton2.setPreferredSize(new Dimension(80,30));
-        imageButton3.setPreferredSize(new Dimension(150,150));
+        imageButton3.setPreferredSize(new Dimension(220,340));
         //imageButton1.setIcon(new ImageIcon(bild1));
         //imageButton1.add(new ImageIcon(bild1));
 
