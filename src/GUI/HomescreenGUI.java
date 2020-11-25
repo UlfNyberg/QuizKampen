b67d74454968d97f58f223564c6dc934750e28f3
UlfNyberg <72010004+UlfNyberg@users.noreply.github.com>
2020-11-25 20:41:55 +0100

    welcomeGUI, first draft

--- src/GUI/HomescreenGUI.java
+++ src/GUI/HomescreenGUI.java
@@ -27,27 +27,28 @@ public class HomescreenGUI extends JPanel {
     public JTextField portNrTextField = new JTextField("22222", 15);
     public JTextField ipAddressTextField = new JTextField("127.0.0.1", 15);
 
-    //ImageIcon bild1 = new ImageIcon("src/GUI/illustration1.jpg");
     ImageIcon bild1 = new ImageIcon(new ImageIcon ("src/GUI/illustration1.jpg").getImage().getScaledInstance(250,850, Image.SCALE_DEFAULT));
     ImageIcon bild2 = new ImageIcon(new ImageIcon ("src/GUI/illustration2.jpg").getImage().getScaledInstance(250,850, Image.SCALE_DEFAULT));
     ImageIcon bild3 = new ImageIcon("src/GUI/illustration3.jpg");
     ImageIcon bild4 = new ImageIcon("src/GUI/illustration4.jpg");
     ImageIcon bild5 = new ImageIcon(new ImageIcon ("src/GUI/logopicture.jpg").getImage().getScaledInstance(220,220, Image.SCALE_DEFAULT));
-    ImageIcon bild6 = new ImageIcon(new ImageIcon ("src/GUI/welcomePicture.jpg").getImage().getScaledInstance(220,340, Image.SCALE_DEFAULT));
+    ImageIcon bild6 = new ImageIcon(new ImageIcon ("src/GUI/welcomePicture.jpg").getImage().getScaledInstance(240,340, Image.SCALE_DEFAULT));
 
+
+
+    ImageIcon bwBrainImage = new ImageIcon(new ImageIcon ("src/GUI/bwBrain.jpg").getImage().getScaledInstance(250,850, Image.SCALE_DEFAULT));
+    ImageIcon yellowBrainImage = new ImageIcon(new ImageIcon ("src/GUI/yellowBrain").getImage().getScaledInstance(250,850, Image.SCALE_DEFAULT));
 
 
     ImageIcon skyBanner1Image = new ImageIcon(new ImageIcon ("src/GUI/skyBanner1.png").getImage().getScaledInstance(250,850, Image.SCALE_DEFAULT));
     ImageIcon skyBanner2Image = new ImageIcon(new ImageIcon ("src/GUI/skyBanner2.png").getImage().getScaledInstance(250,850, Image.SCALE_DEFAULT));
     ImageIcon gameNameBannerImage = new ImageIcon(new ImageIcon ("src/GUI/gameNamebanner.png").getImage().getScaledInstance(150,150, Image.SCALE_DEFAULT));
     ImageIcon gameNameCenterImage = new ImageIcon(new ImageIcon ("src/GUI/GameNameCenter.png").getImage().getScaledInstance(250,250, Image.SCALE_DEFAULT));
-    ImageIcon gameNameTopImage = new ImageIcon(new ImageIcon ("src/GUI/gameNameTop.png").getImage().getScaledInstance(250,250, Image.SCALE_DEFAULT));
+    ImageIcon gameNameTopImage = new ImageIcon(new ImageIcon ("src/GUI/gameNameTopResized.png").getImage().getScaledInstance(400,600, Image.SCALE_SMOOTH));
 
 
-    JLabel imagePanel = new JLabel(new ImageIcon("src/GUI/gameNameTop.png"));
+    JLabel imagePanel = new JLabel(new ImageIcon("src/GUI/gameNameTopResized.png"));
 
-
-    //JButton imageButton1 = new JButton(new ImageIcon("src/GUI/illustration1.jpg"));
     JButton imageButton1 = new JButton(bild1);
     JButton imageButton2 = new JButton(bild2);
     JButton imageButton3 = new JButton(bild6);
@@ -56,14 +57,18 @@ public class HomescreenGUI extends JPanel {
     public HomescreenGUI(ActionListener al) {
         this.setLayout(new BorderLayout());
         panel.setLayout(new BorderLayout());
+        //panel.add(imagePanel);
         add(panel);
-        welcomeLabel.setFont(new Font ("", Font.PLAIN, 30));
-        //panel.add(welcomeLabel, BorderLayout.NORTH);
+
+
+        //welcomeLabel.setFont(new Font ("", Font.PLAIN, 30));
+        //panel.add(imageButton3, BorderLayout.NORTH);
         panel.add(initiateNewGameButton, BorderLayout.SOUTH);
-        panel.add(imageButton1, BorderLayout.WEST);welcomeLabel.setSize(30,30);
+        panel.add(imageButton1, BorderLayout.WEST);
+        //welcomeLabel.setSize(30,30);
         panel.add(imageButton2, BorderLayout.EAST);
         panel.add(panel2, BorderLayout.CENTER);
-        panel2.add(imageButton3);
+        panel2.add(imageButton3, BorderLayout.NORTH);
         //panel2.add(welcomeLabel);
         panel2.add(nameOfPlayerLabel);
         panel2.add(nameOfPlayerTextField);
@@ -72,10 +77,9 @@ public class HomescreenGUI extends JPanel {
         panel2.add(portNrLabel);
         panel2.add(portNrTextField);
         //panel2.add(imageButton3);
-        //panel.add(new ImageIcon(gameNameTopImage));
         imageButton1.setPreferredSize(new Dimension(80,50));
         imageButton2.setPreferredSize(new Dimension(80,30));
-        imageButton3.setPreferredSize(new Dimension(220,340));
+        imageButton3.setPreferredSize(new Dimension(240,340));
         //imageButton1.setIcon(new ImageIcon(bild1));
         //imageButton1.add(new ImageIcon(bild1));
 
@@ -88,5 +92,7 @@ public class HomescreenGUI extends JPanel {
         setSize(400, 600);
         panel2.setBackground(Color.PINK);
         panel.setBackground(Color.PINK);
+
+
     }
 }
---
+++ src/GUI/WelcomeGUI.java
@@ -0,0 +1,68 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ * Created by: Ulf Nyberg
+ * Date: 2020-11-25
+ * Time: 19:47
+ * Project: QuizKampen
+ * Copyright: MIT
+ */
+public class WelcomeGUI extends JFrame{
+
+
+    JPanel panel = new JPanel();
+    JPanel panel2 = new JPanel();
+
+    ImageIcon welcomeImage = new ImageIcon(new ImageIcon ("src/GUI/welcomePicture.jpg").getImage().getScaledInstance(400,600, Image.SCALE_DEFAULT));
+    ImageIcon welcome2Image = new ImageIcon(new ImageIcon ("src/GUI/welcome2Picture.jpg").getImage().getScaledInstance(400,600, Image.SCALE_DEFAULT));
+
+
+    JButton imageButton1 = new JButton(welcomeImage);
+
+    JLabel imagePanel = new JLabel(welcomeImage);
+    JLabel image2Panel = new JLabel(welcome2Image);
+
+    public WelcomeGUI (){  //ActionListener al
+        this.setLayout(new BorderLayout());
+        panel2.setLayout(new BorderLayout());
+        panel.add(imagePanel);
+        panel2.add(image2Panel);
+        add(panel2);
+        add(panel);
+
+        setSize(400, 620);
+        setVisible(true);
+
+
+        imagePanel.addMouseListener(new MouseAdapter() {
+                                        public void mouseEntered(MouseEvent evt) {
+                                            add(panel2);
+                                            panel.setVisible(false);
+                                        }
+
+                                        public void mouseExited(MouseEvent evt) {
+                                            //add(panel);
+                                            panel.setVisible(true);
+                                            //panel2.setVisible(false);
+                                            //panel.setVisible(true);
+                                        }
+                                    });
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        WelcomeGUI wq = new WelcomeGUI();
+    }
+
+
+}
