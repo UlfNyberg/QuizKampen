77fd4c620eeb02f101ef3ad7e4099de1e0bf928f
Elliotkongu <elliot.falt@gmail.com>
2020-12-02 10:34:40 +0100

    Optimiserade import på FileImporter

--- src/Util/FileImporter.java
+++ src/Util/FileImporter.java
@@ -4,10 +4,11 @@ import NetworkClasses.Answer;
 import NetworkClasses.Question;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * Created by Elliot Åberg Fält
